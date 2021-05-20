20f5f1fd4c8e4ae5ce304a434cf90863f9c9487a
Ennkaos <xenon_1q2w@yahoo.com>
2021-05-20 12:16:56 +0200

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
-
+const getLocalStorage=()=>{
+  let list=localStorage.getItem("list");
+  if(list){
+    return JSON.parse(localStorage.getItem("list"))
+  }else{
+    return []
+  }
+}
 function App() {
   const [name,setName]=useState('')
-  const [list,setList]=useState([])
+  const [list,setList]=useState(getLocalStorage())
   const [isEditing,setisEditing]=useState(false)
   const [editId,seteditId]=useState(null)
   const [alert,setAlert]=useState({show:false,msg:'hello',type:'success'})
@@ -51,6 +58,9 @@ function App() {
     showAlert(true,"danger","Item Removed")
     setList(list.filter((item)=>item.id!==id))
   }
+  useEffect(()=>{
+    localStorage.setItem("list",JSON.stringify(list))
+  },[list])
   return <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
         {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
